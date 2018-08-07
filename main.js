610dec3584289d21a047c0a7ab078f58f99959ac
teredithnf <tere.navarro85@gmail.com>
2018-08-07 13:01:58 -0500

    add json a main

---
+++ main.js
@@ -0,0 +1,59 @@
+var dataRestaurantes ={
+    "restaurantes":[
+        {"nombre":"Central Restaurant",
+        "direccion":" Jirón 2 de Mayo 253",
+        "distrito" : "Barranco 15063",
+        "Teléfono": "(01) 2428515",
+        "tipo": "peruana"
+        },
+        {"nombre":"Isolina",
+        "direccion":" Av San Martin 101",
+        "distrito": "Miraflores",
+        "Teléfono": "(01) 2425678",
+        "tipo": "peruana"
+        }, 
+        {"nombre":"Amoramar",
+        "direccion":" Jirón 2 de Mayo 253, Barranco 15063",
+        "distrito" : "surco 1506",
+        "Teléfono": "(01) 25498505",
+        "tipo": "mexicana"
+        },
+        {"nombre":"El Charro",
+        "direccion":" Jirón 2 de Mayo 253, Barranco 15063",
+        "distrito" : "San bORJA 163",
+        "Teléfono": "(01) 68909273",
+        "tipo": "mexicana"
+        },
+         {"nombre":"Rigoletto",
+        "direccion":"Calle Colón 161, Miraflores 15074",
+        "distrito" : "Barranco 15063",
+        "Teléfono": "(01) 5741053",
+        "tipo": "italiana"
+        },
+         {"nombre":"La 73",
+        "direccion":"  Av. el Sol 1756",
+        "distrito" : "Lima",
+        "Teléfono": "(01) 97655815",
+        "tipo": "italiana"
+        },
+         {"nombre":"Sóngoro Cosongo",
+        "direccion":" Jirón Ayacucho 281",
+        "distrito" : "La Victoria 153",
+        "Teléfono": "(01) 22671738",
+        "tipo": "criolla"
+        },
+         {"nombre":"Veggie Pizza",
+        "direccion":" Jr. Colina 112",
+        "distrito" : "Barranco ",
+        "Teléfono": "(01) 36384408",
+        "tipo": "vegana"
+        }, 
+        {"nombre":"Fridays Larcomar",
+        "direccion":" Larcomar, Malecón de la Reserva 610",
+        "distrito" : "Miraflores",
+        "Teléfono": "(01) 7953638",
+        "tipo": "comida rapida"
+        },
+       
+    ]
+}
