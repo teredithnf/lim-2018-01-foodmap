2a52b735a6d2ccc2df9249ae7fbed4bf2646d58e
teredithnf <tere.navarro85@gmail.com>
2018-08-07 11:21:06 -0500

    guardando links para busqueda

--- mapas.js
+++ mapas.js
@@ -33,3 +33,10 @@ google.maps.event.addListener(searchBox, 'places_changed', function() {
     map.setZoom(15);
 });
 
+// https://maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters
+
+// https://maps.googleapis.com/maps/api/place/findplacefromtext/json?parameters
+
+// https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mongolian%20grill&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@47.6918452,-122.2226413&key=YOUR_API_KEY
+
+// https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=YOUR_API_KEY
